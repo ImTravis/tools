4e2ae9a0207b73bda50145dfdfc8aa514aca7c40
1139539742@qq.com <1139539742@qq.com>
2020-03-20 17:42:20 +0800

    HttpClient

--- src/test/java/com/tools/ToolsApplicationTests.java
+++ src/test/java/com/tools/ToolsApplicationTests.java
@@ -102,7 +102,7 @@ public class ToolsApplicationTests {
 		// 结果集
 		List<String> list = new ArrayList<>();
 
-		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
 
 		List<Integer> taskList = Arrays.asList(2, 1, 3, 4, 5, 6, 7, 8, 9, 10);
 		// 全流式处理转换成CompletableFuture[]+组装成一个无返回值CompletableFuture，join等待执行完毕。返回结果whenComplete获取
@@ -110,13 +110,12 @@ public class ToolsApplicationTests {
 				.map(integer -> CompletableFuture.supplyAsync(() -> calc(integer), executorService)
 						.thenApply(h->Integer.toString(h))
 						.whenComplete((s, e) -> {
-							System.out.println("任务"+s+"完成!result="+s+"，异常 e="+e+","+new Date());
 							list.add(s);
 						})
 				).toArray(CompletableFuture[]::new);
 		// 封装后无返回值，必须自己whenComplete()获取
 		CompletableFuture.anyOf(cfs).join();
-		System.out.println("list="+list+",耗时="+(System.currentTimeMillis()-start));
+		System.out.println("\nlist="+list+",耗时="+(System.currentTimeMillis()-start));
 	}
 
 	public int calc(Integer i) {
@@ -128,8 +127,8 @@ public class ToolsApplicationTests {
 			} else {
 				Thread.sleep(1000);//其它任务耗时1秒
 			}
-			System.out.println("task线程：" + Thread.currentThread().getName()
-					+ "任务i=" + i + ",完成！+" + new Date());
+			System.out.println("\nTASK线程：" + Thread.currentThread().getName()
+					+ "任务i=" + i + ",完成");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
